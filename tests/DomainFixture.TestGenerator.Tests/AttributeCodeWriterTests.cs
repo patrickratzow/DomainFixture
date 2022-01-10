b2dc4b665ae2b881389e94070a9d894d8a8e59c8
Patrick Ratzow <patrickratzow@hotmail.dk>
2022-01-10 16:26:46 +0100

    chore: remove .idea directory

---
+++ tests/DomainFixture.TestGenerator.Tests/AttributeCodeWriterTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using DomainFixture.TestGenerator.Framework;
+using FluentAssertions;
+using NUnit.Framework;
+using Environment = System.Environment;
+
+namespace DomainFixture.TestGenerator.Tests;
+
+[TestFixture]
+public class AttributeCodeWriterTests
+{
+    private AttributeCodeWriter _writer = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _writer = new();
+    }
+    
+    [Test]
+    public void ToString_ShouldWriteAttribute_WhenGivenOneAttribute()
+    {
+        // Arrange
+        var attributes = new List<Attribute>
+        {
+            new("Test", "TestNamespace")
+        };
+        _writer.AddAttributes(attributes);
+
+        // Act
+        var result = _writer.ToString();
+
+        // Assert
+        result.Should().Be("[Test]");
+    }
+    
+    [Test]
+    public void ToString_ShouldWriteAttributeOnMultipleLines_WhenGivenMoreThanOneAttribute()
+    {
+        // Arrange
+        var attributes = new List<Attribute>
+        {
+            new("Test", "TestNamespace"),
+            new("Category", "TestNamespace", new() { "\"User\"" })
+        };
+        _writer.AddAttributes(attributes);
+
+        // Act
+        var result = _writer.ToString();
+
+        // Assert
+        result.Should().Be($"[Test]{Environment.NewLine}[Category(\"User\")]");
+    }
+}
